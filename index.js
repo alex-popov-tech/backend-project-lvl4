e1d2777fe28a7c5f463f1f9c19d846c044a080b8
Alexander Popov <alex.popov.tech@gmail.com>
2020-07-16 01:41:29 +0300

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+import Koa from 'koa';
+import Rollbar from 'rollbar';
+
+dotenv.config();
+const app = new Koa();
+const rollbar = new Rollbar({
+  accessToken: process.env.ROLLBAR_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+
+app.on('error', err => rollbar.error(err));
+app.use(async ctx => {
+  throw new Error('hello there');
+});
+
+app.listen(3000);
