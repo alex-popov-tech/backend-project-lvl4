35a5fffc37e664f1c59737c36c3111be9006b277
Alexander Popov <alex.popov.tech@gmail.com>
2021-04-26 23:35:01 +0300

    delete extra logs

--- server/bin/index.js
+++ server/bin/index.js
@@ -1,15 +1,10 @@
 import fastify from '..';
 
 (async () => {
-  console.log('before create app');
   const app = fastify();
-  console.log('after create app');
   try {
-    console.log('before start app');
     await app.listen(process.env.PORT || 3000, process.env.HOST || '127.0.0.1');
-    console.log(`server listening on ${app.server.address().address}:${app.server.address().port}`);
   } catch (err) {
-    console.log('before throw error');
     app.log.error(err);
     process.exit(1);
   }
